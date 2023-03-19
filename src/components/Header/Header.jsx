1a05e06a71967de88eea6943c1beb4fad2e4bd34
michalmosior <michal.mosior@gmail.com>
2023-03-19 12:40:05 +0100

    added background change in conjunction with theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import styles from './Header.module.css';
 import ThemeButton from '../ThemeButton/ThemeButton';
 
 const Header = ({ changeTheme, theme }) => {
+	const headerBackground = theme ? styles.headerlight : styles.headerdark;
 	return (
-		<header className={`${styles.header} ${styles.headerlight}`}>
+		<header className={`${styles.header} ${headerBackground}`}>
 			<h1>
 				To Do
 				<ThemeButton action={changeTheme} theme={theme} />
